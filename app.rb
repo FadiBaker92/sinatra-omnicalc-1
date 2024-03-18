4adbd9753e03e723467f2503d86977f425ccd3a6
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-18 19:39:44 +0000

    created 3 erb files square, square_root,square_root_results.erb. Also added the html and worked on functionality

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-erb(:homepage)
+  erb(:homepage)
 end
 
 get("/square/new") do
   erb(:square_new)
 end
 
+get("/square_root/new")do
+  erb(:square_root_new)
+end
+
 get("/square/results") do
   erb(:square_results)
+end
+
+get("/square_root/results")do 
+  erb(:square_root_results)
 end
