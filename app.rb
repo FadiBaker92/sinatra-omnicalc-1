bf7b322f6b701d9b69c0b87938691e785d13a662
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-19 01:30:45 +0000

    added a form element to random.erb and html to random_results

--- app.rb
+++ app.rb
@@ -30,8 +30,17 @@ get("/random/new")do
 end
 
 get("/payment/results")do
+  @user_apr = params.fetch("apr").to_f 
+  @present_value = params.fetch("principal").to_f
+  @num_years = params.fetch("num_of_years").to_i
+  @rate = @user_apr / 100
+  @monthly_rate = @rate / 12
+  @num_of_periods = @num_years.to_f / 12
+  @numerator = @monthly_rate * @present_value
+  @denominator = 1 - ((1 + @monthly_rate) ** (-@num_of_periods))
   erb(:payment_results)
 end
 get("/random/results")do
+  @random_num = rand(params.fetch('minimum').to_f..params.fetch('maximum').to_f)
   erb(:random_results)
 end
